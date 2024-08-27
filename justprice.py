1e48046cffee1c426209ceae03597695619403a2
ordi2214217 <ordi2214217@educ.normandie.fr>
2024-08-27 15:03:09 +0200

    petite modification

--- justprice.py
+++ justprice.py
@@ -1,19 +1,22 @@
 import random
 
 print("Trouver le prix de l'objet X..")
-cible = random.randit(1.10)
+cible = (random.randint(1,10))
 # print(f"cible={cible}")
 
-essai = int(input("Votre proposition entre 1 et 10 : "))
+for i in range (5):
+    essai = int(input("Votre proposition entre 1 et 10 : "))
 
-if essai < 1 or essai > 10:
-    print("Erreur,il faut saisir une valeur entre 1 et 10...")
-    exit()
+    if essai < 1 or essai > 10 :
+        print("Erreur,il faut saisir une valeur entre 1 et 10...")
+        exit()
 
-if cible == essai:
-    print("Bravo!!!")
+    if cible == essai:
+        print("Bravo!!!")
+        break
+    elif cible < essai:
+        print("Trop élevé...")
 
-elif cible < essai:
-    print("Trop élevé...")
-else:
-    print("Trop peu ...")
+    else:
+        print("Trop peu ...")
+print ("Fin du jeu...")
